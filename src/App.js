3d38bc712e8a8efd2ae1c1294570235e8609a0ee
Demanuel001 <daniloemanuel360@gmail.com>
2021-09-18 17:35:16 -0300

    projeto desenvolvido durante a maratona js 3 do Emerson Broga

--- src/App.js
+++ src/App.js
@@ -1,20 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import wordList from './resources/words.json';
+
+const MAX_TYPED_KEYS = 30;
+
+const getWord = () => {
+    const index = Math.floor( Math.random() * wordList.length );
+    const word = wordList[index];
+    return word.toLowerCase();
+};
+
+const isValidKey = (key, word) => {
+    if(!word) return false;
+    const result = word.split('').includes(key);
+    return result;
+}
+
+const Word = ({word, validKeys}) => {
+    if(!word) return null;
+    const joinedKeys = validKeys.join('');
+    const matched = word.slice(0,joinedKeys.length);
+    const remainder = word.slice(joinedKeys.length);
+    return (<>
+        <span className="matched">{matched}</span>
+        <span className="remainder">{remainder}</span>
+    </>);
+};
 
 const App = () => {
-    return (<div className="container">
-        <div className="valid-keys">
-            <span className="matched">emer</span>
-            <span className="remainder">son</span>
+    const [typedKeys, setTypedKeys] = useState([]);
+    const [validKeys, setValidKeys] = useState([]);
+    const [completedWords, setCompletedWords] = useState([]);
+    const [word, setWord] = useState('');
+
+    useEffect(()=>{
+        setWord(getWord());
+    }, []);
+
+    useEffect(()=>{
+        const wordFromValidKeys = validKeys.join('').toLowerCase();
+        if( word && word === wordFromValidKeys){
+            let newWord = null;
+            do {
+                newWord = getWord();
+            } while(completedWords.includes(newWord));
+
+            setWord(newWord);
+            setValidKeys([]);
+            setCompletedWords((prev) => [...prev, word]);
+        }
+    }, [word, validKeys, completedWords]);
+
+    const handleKeyDown = (e) => {
+        e.preventDefault();
+        const { key } = e;
+        setTypedKeys((prev) => [...prev, key].slice(MAX_TYPED_KEYS * -1));
+
+        if(isValidKey(key, word)) {
+            setValidKeys((prev) => {
+                const isValidLenght = prev.length <= word.length;
+                const isNextChar = isValidLenght && word[prev.length] === key;
+                return (isNextChar) ? [...prev, key] : prev;
+            });
+        };
+    };
+
+    return (
+        <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
+            <div className="valid-keys">
+                <Word word={word} validKeys={validKeys} />
+            </div>
+            <div className="typed-keys">{ typedKeys ? typedKeys.join(' ') : null }</div>
+            <div className="completed-words">
+                <ol>
+                    {completedWords.map((word)=>{
+                        return (<li key={word}>{word}</li>)
+                    })}
+                </ol>
+            </div>
         </div>
-        <div className="typed-keys">asdfemerasdf</div>
-        <div className="completed-words">
-            <ol>
-                <li>Cidade</li>
-                <li>Estado</li>
-                <li>País</li>
-            </ol>
-        </div>
-    </div>);
+    );
 };
 
 export default App;
